ad9b3d7fbc8cb1e628ae07424caec0a221711866
Jahaziel Hernandez Hoyos <jahazielhernandezhoyoz@gmail.com>
2022-05-25 10:11:13 -0500

    cambios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import clienteAxios from './config/axios';
+
+// Componentes
+import Pacientes from './components/Pacientes';
+import NuevaCita from './components/NuevaCita';
+import Cita from './components/Cita';
+
 
 function App() {
+
+  // State de la app
+  const [citas, guardarCitas] = useState([]);
+  const [consultar, guardarConsultar] = useState(true);
+
+  useEffect( () => {
+      if(consultar) {
+        const consultarAPI = () => {
+            clienteAxios.get('/pacientes')
+              .then(respuesta => {
+                  // colocar en el state el resultado
+                  guardarCitas(respuesta.data);
+
+                  // deshabilitar la consulta
+                  guardarConsultar(false);
+              })
+              .catch(error => {
+                console.log(error)
+              })
+        }
+        consultarAPI();
+      }
+  }, [consultar] );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+          <Switch>
+              <Route 
+                  exact 
+                  path="/"
+                  component={() => <Pacientes citas={citas} />}
+              />
+
+              <Route 
+                exact 
+                path="/nueva"
+                component={() => <NuevaCita guardarConsultar={guardarConsultar}  />}
+              />
+
+              <Route 
+                  exact 
+                  path="/cita/:id"
+                  render={(props) => {
+                      const cita = citas.filter(cita => cita._id === props.match.params.id)
+
+                      return (
+                        <Cita 
+                          cita={cita[0]}
+                          guardarConsultar={guardarConsultar}
+                        />
+                      )
+                  }}
+              />
+          </Switch>
+      </Router>
   );
 }
 
